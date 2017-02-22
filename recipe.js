ab1916641f38c0cd3d9cb58c55603093fdf66a1e
Ray Linsenmayer <raymond_linsenmayer@hotmail.com>
2017-02-22 13:15:33 +0000

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,17 @@
 'use strict';
+var recipe = {};
+
+function addIngredient(recipe, name, amount) {
+	recipe[name] = amount;
+  	return recipe;
+}
+
+function removeIngredient(recipe, ingredient){
+  delete recipe[ingredient];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, amount){
+  recipe[ingredient] = amount;
+  return recipe;
+}
